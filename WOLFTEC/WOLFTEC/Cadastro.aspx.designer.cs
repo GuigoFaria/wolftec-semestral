8f799a6d698a7d85da61dae24926ed07ac94610b
Bruno Carvalho <37667964+papamike18@users.noreply.github.com>
2018-05-22 17:55:49 -0300

    gui mandou subi

--- WOLFTEC/WOLFTEC/Cadastro.aspx.designer.cs
+++ WOLFTEC/WOLFTEC/Cadastro.aspx.designer.cs
@@ -65,5 +65,32 @@ namespace WOLFTEC {
         /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
         /// </remarks>
         protected global::System.Web.UI.WebControls.TextBox Senha;
+        
+        /// <summary>
+        /// Controle Nome_Server.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox Nome_Server;
+        
+        /// <summary>
+        /// Controle Temp_Maxima.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox Temp_Maxima;
+        
+        /// <summary>
+        /// Controle Temp_Minima.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox Temp_Minima;
     }
 }
